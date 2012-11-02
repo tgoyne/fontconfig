f21aafe17cadd9b1e2cd0bd20a0006dbcfd21ce3
Thomas Goyne <tgoyne@gmail.com>
2012-11-02 07:51:13 -0700

    Add msvc project and static versions of generated files

---
+++ win32/include/config.h
@@ -0,0 +1,45 @@
+#define HAVE_CHSIZE 1
+#define HAVE_FCNTL_H 1
+#define HAVE_FT_BITMAP_SIZE_Y_PPEM 1
+#define HAVE_FT_GET_NEXT_CHAR 1
+#define HAVE_FT_GET_PS_FONT_INFO 1
+#define HAVE_FT_HAS_PS_GLYPH_NAMES 1
+#define HAVE_FT_SELECT_SIZE 1
+//#define HAVE_FT_GET_BDF_PROPERTY 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_MEMMOVE 1
+
+#define HAVE_MEMORY_H 1
+#define HAVE_MEMSET 1
+#define HAVE_RAND 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRCHR 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRRCHR 1
+#define HAVE_STRTOL 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_VPRINTF 1
+#define STDC_HEADERS 1
+#define USE_ICONV 0
+
+#define FLEXIBLE_ARRAY_MEMBER 1
+
+// Don't include mingw unistd.h
+#define _UNISTD_H
+
+#define VERSION "2.10.1"
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+
+#undef UNICODE
+#undef _UNICODE
+#define MBCS
+#define _MBCS
+
+#ifndef __cplusplus
+/* #undef inline */
+#endif
